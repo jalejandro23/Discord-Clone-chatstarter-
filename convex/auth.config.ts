c3e974a95e5e1f2caa74c6b49c59605458fe3d18
jalejandro23 <jaydenalejandro12@gmail.com>
2024-12-31 13:56:49 -0500

    Init Clerk

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://communal-barnacle-32.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
 "use client";
-import { useMutation, useQuery } from "convex/react";
+
+import {
+  Authenticated,
+  Unauthenticated,
+  useMutation,
+  useQuery,
+} from "convex/react";
+
 import { useState } from "react";
 import { api } from "../../convex/_generated/api";
+import { SignInButton } from "@clerk/nextjs";
 
 export default function Home() {
   const messages = useQuery(api.functions.message.list);
@@ -12,24 +20,31 @@ export default function Home() {
     createMessage({ sender: "Alice", content: input });
     setInput("");
   };
-  return (
-    <div>
-      {messages?.map((message, index) => (
-        <div key={index}>
-          <strong>{message.sender}</strong>: {message.content}
-        </div>
-        ))}
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            name="message"
-            id="message"
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-          />
-          <button type="submit">Send</button>
-        </form>
-      </div>
-    );
-  }
 
+  return (
+    <>
+      <Authenticated>
+        <div>
+          {messages?.map((message, index) => (
+            <div key={index}>
+              <strong>{message.sender}</strong>: {message.content}
+            </div>
+          ))}
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              name="message"
+              id="message"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+            />
+            <button type="submit">Send</button>
+          </form>
+        </div>
+      </Authenticated>
+      <Unauthenticated>
+        <SignInButton />
+      </Unauthenticated>
+    </>
+  );
+}
